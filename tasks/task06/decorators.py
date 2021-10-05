d4182d69fff901f8c5a34c4cccb81446e2665ba0
smasalovich <smasalovich@barracuda.com>
2021-10-05 13:58:40 +0300

    add changes

--- tasks/task06/decorators.py
+++ tasks/task06/decorators.py
@@ -3,18 +3,22 @@ import timeit
 
 def measure_elapsed_time(func):
     def wrapper(*args, **kwargs):
-        t = timeit.timeit(lambda: func(*args, **kwargs))
-        print(f'{func.__name__} call took: {t:.2f} sec')
-        print(func.__name__+' result:', func(*args, **kwargs))
+        start = timeit.default_timer()
+        # t = timeit.timeit(lambda: func(*args, **kwargs))
+        func(*args, **kwargs)
+        end = timeit.default_timer()
+        t = end-start
+        print(f'{func.__name__} call took: {t:.6} sec')
+
     return wrapper
 
 def n_plus_1(n):
     result = n+1
-    return result
+    print('n_plus_1 result:', result)
 
 def n_mines_1(n):
     result = n-1
-    return result
+    print('n_mines_ result:', result)
 
 plus1 = measure_elapsed_time(n_plus_1)
 mines1 = measure_elapsed_time(n_mines_1)
